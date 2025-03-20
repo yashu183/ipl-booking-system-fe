5146076e857dd88e18a115a874c4287c55c816b7
Pavithra-fireflies14 <pavithrameganathan0001@gmail.com>
2025-03-20 19:36:35 +0530

    Bookings flow - initial commit (Get all bookings, cancel booking

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header-wrapper">
                 <Logo />
-
                 <nav className="nav">
-                    <a href="/" className="nav-link">Home</a>
-                    <a href="/history" className="nav-link">Booking history</a>
-
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/history" className="nav-link">Booking History</Link>
                     <a href="#" className="user-avatar">CY</a>
                 </nav>
             </div>
